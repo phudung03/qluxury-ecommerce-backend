0b5430f490fa528e5d7c122a6ed863f489a07bfc
phudung03 <npdung2003@gmail.com>
2025-11-07 15:37:20 +0700

    Tao du an moi

---
+++ src/main/java/com/qluxury/qluxury_ecommerce_backend/QluxuryEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qluxury.qluxury_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QluxuryEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QluxuryEcommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qluxury/qluxury_ecommerce_backend/QluxuryEcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qluxury.qluxury_ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QluxuryEcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
